456850a5300e53c90c4aa26ed6663a64959e906f
Alwi Sahrul Ramadhan <89216230+alwiramadhan16@users.noreply.github.com>
2021-09-18 05:28:03 +0700

    Add files via upload

---
+++ koneksi.php
@@ -0,0 +1,13 @@
+
+
+<?php
+$servername = "localhost";
+$database = "uts-pl2_adit";
+$username = "root";
+$password = "";
+
+
+$conn = mysqli_connect($servername, $username, $password, $database);
+
+return $conn;
+?>
